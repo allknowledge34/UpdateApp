7c962cfba5f51c5a20fd8680bc342a58222e26a1
sachinkumar <allknowledge34@gmail.com>
2025-11-08 16:58:39 +0530

    First commit

---
+++ app/src/main/java/com/example/updateapp/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.updateapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.updateapp.databinding.ActivityMainBinding;
+import com.example.updateapp.views.fragments.ProfileFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.txt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ProfileFragment fragment = new ProfileFragment();
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.fragment_container, fragment)
+                        .addToBackStack(null)
+                        .commit();
+            }
+        });
+    }
+}
